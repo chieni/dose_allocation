b2ffd22330c337e916c8fd374ebc788e97b6c70f
Isabel Chien <ic390@Administrators-MacBook-Pro.local>
2022-10-25 15:06:15 +0100

    approximate gp implemented'

--- gp.py
+++ gp.py
@@ -7,6 +7,7 @@ import gpytorch
 from matplotlib import pyplot as plt
 
 
+torch.manual_seed(0)
 # We will use the simplest form of GP model, exact inference
 class ExactGPModel(gpytorch.models.ExactGP):
     def __init__(self, train_x, train_y, likelihood):
@@ -88,18 +89,18 @@ def regression_example():
 
 
 
-
-
-class ClassificationGPModel(gpytorch.models.ExactGP):
-    def __init__(self, train_x, train_y, likelihood):
-        super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
+class ClassificationGPModel(gpytorch.models.ApproximateGP):
+    def __init__(self, inducing_points):
+        variational_distribution = gpytorch.variational.CholeskyVariationalDistribution(inducing_points.size(0))
+        variational_strategy = gpytorch.variational.VariationalStrategy(self, inducing_points, variational_distribution, learn_inducing_locations=True)
+        super(ClassificationGPModel, self).__init__(variational_strategy)
         self.mean_module = gpytorch.means.ConstantMean()
         self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
     
     def forward(self, x):
         mean_x = self.mean_module(x)
         covar_x = self.covar_module(x)
-
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
 
 def classification_example():
@@ -107,32 +108,31 @@ def classification_example():
     # train_y = torch.sign(torch.cos(train_x * (4 * math.pi)))
 
     train_x = torch.arange(0, 5, 0.2)
-    a = torch.sin(train_x * torch.pi * 0.5) * 2
-    train_y = torch.bernoulli(torch.sigmoid(a))
+    train_y = torch.zeros(len(train_x))
+    train_y[:int(len(train_x) / 2)] = 1.
+    # a = torch.sin(train_x * torch.pi * 0.5) * 2
+    # train_y = torch.bernoulli(torch.sigmoid(a))
 
-    model, likelihood = train(train_x, train_y)
+    model, likelihood = train_classifier(train_x, train_y)
     predict_classifier(model, likelihood, train_x, train_y)
 
 def train_classifier(train_x, train_y):
-    likelihood = gpytorch.likelihoods.BetaLikelihood()
-    model = ClassificationGPModel(train_x, train_y, likelihood)
-    training_iter = 50
+    model = ClassificationGPModel(train_x)
+    likelihood = gpytorch.likelihoods.BernoulliLikelihood()
+    training_iter = 100
 
     model.train()
     likelihood.train()
-
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.1) 
-    mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
+    optimizer = torch.optim.Adam([{'params': model.parameters()}, {'params': likelihood.parameters()},], lr=0.01)
+    mll = gpytorch.mlls.VariationalELBO(likelihood, model, num_data=train_x.size(0), beta=1.)
 
     for i in range(training_iter):
         optimizer.zero_grad()
         output = model(train_x)
         loss = -mll(output, train_y)
         loss.backward()
-        print('Iter %d/%d - Loss: %.3f   lengthscale: %.3f   noise: %.3f' % (
-            i + 1, training_iter, loss.item(),
-            model.covar_module.base_kernel.lengthscale.item(),
-            model.likelihood.noise.item()
+        print('Iter %d/%d - Loss: %.3f' % (
+            i + 1, training_iter, loss.item()
         ))
         optimizer.step()
     return model, likelihood
