e0d1551aad9e27b072e0e18f0815d4a1d3522a2a
chieni <chieni@mit.edu>
2023-02-02 14:10:37 +0800

    scenario 6, lr

--- comparison_plots.py
+++ comparison_plots.py
@@ -135,19 +135,19 @@ def tox_eff_plot(c3t_folder_name, gp_tox_filename, gp_eff_filename):
     sns.scatterplot(data=frame, x='eff', y='tox', hue='method', style='index')
     plt.show()
 
-folder_name = "eleventh_pass"
+folder_name = "thirteenth_pass"
 dose_filename = f"results/{folder_name}/final_dose_error.csv"
 thall_filename = f"results/{folder_name}/thall_final_dose_error_retrain.csv"
 safety_filename = f"results/{folder_name}/safety_violations.csv"
 tox_filename = f"results/{folder_name}/tox_outcome.csv"
 eff_filename = f"results/{folder_name}/eff_outcome.csv"
-utility_filename = f"results/{folder_name}/test_thall_utility.csv"
+utility_filename = f"results/{folder_name}/utility.csv"
 
-c3t_folder_name = "c3t_more6"
+c3t_folder_name = "c3t_more8"
 
-dose_error_plot(c3t_folder_name, thall_filename)
 #safety_plot(c3t_folder_name, safety_filename)
-#utility_plot(c3t_folder_name, utility_filename)
+#dose_error_plot(c3t_folder_name, dose_filename)
 #tox_plot(c3t_folder_name, tox_filename)
+utility_plot(c3t_folder_name, utility_filename)
 #eff_plot(c3t_folder_name, eff_filename)
-# tox_eff_plot(c3t_folder_name, tox_filename, eff_filename)
+#tox_eff_plot(c3t_folder_name, tox_filename, eff_filename)
--- copy_files.py
+++ copy_files.py
@@ -67,5 +67,5 @@ def combine_files(filepath):
             metric_frame[f"scenario{scenario}"] = frames[scenario-1][metric].values
         metric_frame.to_csv(f"{filepath}/{metric}.csv")
 
-copy_files('thirteenth_pass', 'exp17', 100, 2)
-combine_files('thirteenth_pass')
+copy_files('fourteenth_pass', 'exp18', 100, 2)
+combine_files('fourteenth_pass')
